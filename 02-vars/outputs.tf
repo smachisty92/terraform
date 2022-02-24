4f7087e19d1a61719a26b13a8837e775fff375ba
csyed <smachisty92@gmail.com>
2022-02-25 00:12:56 +0530

    terraform

--- 02-vars/outputs.tf
+++ 02-vars/outputs.tf
@@ -5,3 +5,15 @@ output "sample" {
 output "sample1" {
   value = var.sample1
 }
+
+output "sample2" {
+  value = var.sample2
+}
+
+output "sample3" {
+  value = var.sample3
+}
+
+output "sample4" {
+  value = var.sample4
+}
--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -3,3 +3,17 @@ variable "sample" {}
 variable "sample1" {
   default = "New World"
 }
+
+variable "sample2" {
+  default = "New World"
+}
+
+variable "sample3" {
+  default = 100
+}
+
+variable "sample4" {
+  default = true
+}
+
+

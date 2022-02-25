4769d1f934e12b7d8fd48f8202a127298a750e50
csyed <smachisty92@gmail.com>
2022-02-25 12:13:09 +0530

    terraform

--- examples/02-sg-with-ec2-with-modules/main.tf
+++ examples/02-sg-with-ec2-with-modules/main.tf
@@ -6,3 +6,11 @@ module "ec2" {
 module "sg" {
   source = "./sg"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "terraform62"
+    key    = "sample/terraform.tfstate"
+    region = "us-east-1"
+  }
+}

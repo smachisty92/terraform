7f194cb87361025ad5c34b4bd5c5bc50fc4afff0
csyed <smachisty92@gmail.com>
2022-02-25 17:05:02 +0530

    terraform

--- roboshop/module/ec2.tf
+++ roboshop/module/ec2.tf
@@ -9,7 +9,7 @@
 #}
 
 
-resource "aws_spot_instance_request" "cheap_worker" {
+resource "aws_spot_instance_request" "ec2" {
   ami           = "ami-0d997c5f64a74852c"
   spot_price    = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_app.id]

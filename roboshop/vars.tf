fc09f49dd2549af7d124e19b29d4057df11c7a10
csyed <smachisty92@gmail.com>
2022-02-28 01:36:41 +0530

    terraform

--- roboshop/vars.tf
+++ roboshop/vars.tf
@@ -2,13 +2,13 @@
 
 
 variable "COMPONENTS" {
-  default = ["cart", "catalogue", "frontend", "mongodb", "payment", "rabbitmq", "redis", "shipping", "user"]
+  default = ["cart", "catalogue", "frontend", "mongodb", "payment", "rabbitmq", "redis", "shipping", "user", "mysql"]
 }
 
 variable "PORTS" {
-  default = [8080,8080,80, 27017, 8080,5672, 6379, 8080, 808]
+  default = [8080,8080,80, 27017, 8080,5672, 6379, 8080, 808, 3306]
 }
 
 variable "MONITOR" {
-  default = ["yes", "yes", "yes", "no", "no", "yes", "no", "no", "yes", "yes"]
+  default = ["yes", "yes", "yes", "no", "yes", "no", "no", "yes", "yes", "no"]
 }

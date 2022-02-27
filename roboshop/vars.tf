2cd433c3edb2d2ccfb1c0fcc4332ca154c22ca5a
csyed <smachisty92@gmail.com>
2022-02-28 01:26:49 +0530

    terraform

--- roboshop/vars.tf
+++ roboshop/vars.tf
@@ -1,9 +1,12 @@
+#currently having issue with mysql
+
+
 variable "COMPONENTS" {
-  default = ["cart", "catalogue", "frontend", "mongodb", "mysql", "payment", "rabbitmq", "redis", "shipping", "user"]
+  default = ["cart", "catalogue", "frontend", "mongodb", "payment", "rabbitmq", "redis", "shipping", "user"]
 }
 
 variable "PORTS" {
-  default = [8080,8080,80, 27017, 3306, 8080,5672, 6379, 8080, 808]
+  default = [8080,8080,80, 27017, 8080,5672, 6379, 8080, 808]
 }
 
 variable "MONITOR" {

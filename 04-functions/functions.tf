7d02502f6745c8f22402468c5662eec83db60318
csyed <smachisty92@gmail.com>
2022-02-28 01:25:22 +0530

    terraform

--- 04-functions/functions.tf
+++ 04-functions/functions.tf
@@ -5,3 +5,13 @@ variable "list" {
 output "number_of_list" {
   value = length(var.list)
 }
+
+#we can use below way to get the value
+output "get_first_value_1" {
+  value = var.list[0]
+}
+
+#or we can use function as element to get the value
+output "get_first_value_2" {
+  value = element(var.list, 0)
+}

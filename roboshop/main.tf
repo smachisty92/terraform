5a0527cb0bf71ecaa04796eccd5f354ea3baf086
csyed <smachisty92@gmail.com>
2022-02-27 16:22:34 +0530

    terraform

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,30 +1,30 @@
-##module "frontend" {
-##  source = "./frontend"
-##}
-##module "cart" {
-##  source = "./cart"
-##}
-##module "catalogue" {
-##  source = "./catalogue"
-##}
-##module "payment" {
-##  source = "./payment"
-##}
-##module "shipping" {
-##  source = "./shipping"
-##}
-module "mysql" {
-  source = "./mysql"
+module "frontend" {
+  source = "./frontend"
 }
-##module "mongodb" {
-##  source = "./mongodb"
-##}
-##module "redis" {
-##  source = "./redis"
-##}
-##module "rabbitmq" {
-##  source = "./rabbitmq"
-##}
-##module "user" {
-##  source = "./user"
-##}
+module "cart" {
+  source = "./cart"
+}
+module "catalogue" {
+  source = "./catalogue"
+}
+module "payment" {
+  source = "./payment"
+}
+module "shipping" {
+  source = "./shipping"
+}
+#module "mysql" {
+#  source = "./mysql"
+#}
+module "mongodb" {
+  source = "./mongodb"
+}
+module "redis" {
+  source = "./redis"
+}
+module "rabbitmq" {
+  source = "./rabbitmq"
+}
+module "user" {
+  source = "./user"
+}

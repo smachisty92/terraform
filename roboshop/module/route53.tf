47f561b82f16f1f9e905ab568149306e9ab21317
csyed <smachisty92@gmail.com>
2022-02-27 15:44:15 +0530

    terraform

--- roboshop/module/route53.tf
+++ roboshop/module/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "www" {
   zone_id = "Z0790856TUOKEKRIWGR2"
-  name    = var.COMPONENT
+  name    = "${var.COMPONENT}-dev"
   type    = "A"
   ttl     = "300"
   records = [aws_spot_instance_request.cheap_worker.private_ip]

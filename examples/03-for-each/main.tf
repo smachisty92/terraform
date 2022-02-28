6015e98ce1d82f1a179cede0bfb2b20e2c0d30a5
csyed <smachisty92@gmail.com>
2022-02-28 12:10:58 +0530

    terraform

--- examples/03-for-each/main.tf
+++ examples/03-for-each/main.tf
@@ -1,14 +1,31 @@
+#variable "COMP" {
+#  default = ["frontend","cart"]
+#  type = set(string)
+#}
+
 variable "COMP" {
-  default = ["frontend","cart"]
-  type = set(string)
+  default = [
+    {
+      name = "frontend"
+      port = 80
+    },
+    {
+      name = "cart"
+      port = 8080
+    }
+  ]
 }
 
 resource "null_resource" "null" {
-  for_each = var.COMP
+#  for_each = var.COMP
+  for_each = {
+    for index, comp in var.COMP :
+    index => comp
+  }
   triggers = {
     abc = timestamp()
   }
   provisioner "local-exec" {
-    command= "echo Component Name = ${each.value}"
+    command= "echo Component Name = ${each.value.name}, Port =${each.value.port}"
   }
 }
